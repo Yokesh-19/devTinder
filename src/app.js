05a51f52a88de021486d7e9ac84abba46b13a5e4
Yokesh M <727722eucs208@skcet.ac.in>
2026-02-11 19:33:47 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -3,26 +3,11 @@ const express = require("express");
 const app = express();
 
 
-//this will handle get call to /user
-app.get("/user", (req,res) =>{              //just an arrow function
+app.get("/user/:userId/:name/:password", (req,res) =>{
+    console.log(req.params);         //just an arrow function
     res.send({firstname:"Yokesh",lastname:"Manoharan"})
 })
 
-//post call
-app.post("/user", (req,res) =>{              //just an arrow function
-    res.send("Data saved in Database");
-})
-
-//delete call
-app.delete("/user", (req,res) =>{              //just an arrow function
-    res.send("Deleted successfully!");
-})
-
-
-//this will match all the HTTP method API calls to /test
-app.use("/test",(req,res) => {
-    res.send("Hello from the Server!");
-})
 
 app.listen(3000, () =>{
   console.log("Server is listening on port 3000..."); 
