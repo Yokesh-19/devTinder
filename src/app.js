dea2cff51d5d4c32f4d2c7eb36cccd2240f71576
Yokesh M <727722eucs208@skcet.ac.in>
2026-02-11 19:16:13 +0530

    Exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,6 +2,24 @@ const express = require("express");
 
 const app = express();
 
+
+//this will handle get call to /user
+app.get("/user", (req,res) =>{              //just an arrow function
+    res.send({firstname:"Yokesh",lastname:"Manoharan"})
+})
+
+//post call
+app.post("/user", (req,res) =>{              //just an arrow function
+    res.send("Data saved in Database");
+})
+
+//delete call
+app.delete("/user", (req,res) =>{              //just an arrow function
+    res.send("Deleted successfully!");
+})
+
+
+//this will match all the HTTP method API calls to /test
 app.use("/test",(req,res) => {
     res.send("Hello from the Server!");
 })
